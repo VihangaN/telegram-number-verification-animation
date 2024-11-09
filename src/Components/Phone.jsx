8c63a769f6e4903401cf4243b9ee75b6943f3d0a
Vihanga nivarthana <vihanganivarthana@gmail.com>
2024-11-10 02:33:28 +0530

    feat: add loading spinner

--- src/Components/Phone.jsx
+++ src/Components/Phone.jsx
@@ -1,125 +1,82 @@
-import {forwardRef, useEffect, useRef} from "react";
+import {forwardRef, useEffect, useRef, useState} from "react";
 import body from '../assets/body.png';
 import btn from '../assets/btn.png';
 import reciever from '../assets/reciever.png';
+import loader from '../assets/loader.gif';
+
 
 const SvgComponent = ({number}) => {
-
     const svgRef = useRef(null);
+    const [loadedCount, setLoadedCount] = useState(0);
+    const totalImages = 10;
 
     useEffect(() => {
         if (svgRef.current && number) {
-            const lastDidit = number.slice(-1)
-            const activeBtnId = `btn-${lastDidit}`;
+            const lastDigit = number.slice(-1);
+            const activeBtnId = `btn-${lastDigit}`;
             const activeBtn = svgRef.current.getElementById(activeBtnId);
             activeBtn?.classList.add("press");
             activeBtn?.addEventListener('animationend', () => activeBtn.classList.remove("press"));
         }
-    }, [number])
+    }, [number]);
+
+    const handleImageLoad = () => {
+        setLoadedCount((prevCount) => prevCount + 1);
+    };
+
     return (
-        <svg
-            id="Layer_2"
-            data-name="Layer 2"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            viewBox="0 0 315.36 316.1"
-            ref={svgRef}
-            className="phone-base"
-        >
-            <g id="Layer_1-2" data-name="Layer 1">
-                <g id="telephone">
-                    <image
-                        id="reciever"
-                        width={1314}
-                        height={441}
-                        style={{transform: "scale(0.24)"}}
-                        xlinkHref={reciever}
-                    />
-                    <image
-                        id="body"
-                        width={1247}
-                        height={1156}
-                        style={{transform: "translate(8.08px, 38.66px) scale(0.24)"}}
-                        xlinkHref={body}
-                    />
-                    <image
-                        id="btn-1"
-                        width={137}
-                        height={142}
-                        style={{transform: "translate(82.81px, 123.59px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
-                    <image
-                        id="btn-2"
-                        width={137}
-                        height={142}
-                        style={{transform: "translate(141.37px, 123.59px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
-                    <image
-                        id="btn-3"
-                        width={138}
-                        height={142}
-                        style={{transform: "translate(199.69px, 123.59px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
-                    <image
-                        id="btn-4"
-                        width={137}
-                        height={142}
-                        style={{transform: "translate(81.85px, 168.23px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
-                    <image
-                        id="btn-5"
-                        width={137}
-                        height={142}
-                        style={{transform: "translate(141.37px, 168.23px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
-                    <image
-                        id="btn-6"
-                        width={138}
-                        height={142}
-                        style={{transform: "translate(200.65px, 168.23px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
-                    <image
-                        id="btn-7"
-                        width={137}
-                        height={142}
-                        style={{transform: "translate(80.89px, 209.75px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
-                    <image
-                        id="btn-8"
-                        width={137}
-                        height={142}
-                        style={{transform: "translate(141.37px, 209.75px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
-                    <image
-                        id="btn-9"
-                        width={137}
-                        height={142}
-                        style={{transform: "translate(201.85px, 209.75px) scale(0.24)"}}
-                        xlinkHref={btn}
-                        className=""
-                    />
+        <div className="phone-wrapper">
+            {loadedCount < totalImages && <div className="loading">
+                <img src={loader} alt="loading" />
+            </div>}
+            <svg
+                id="Layer_2"
+                data-name="Layer 2"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 315.36 316.1"
+                ref={svgRef}
+                className="phone-base"
+            >
+                <g id="Layer_1-2" data-name="Layer 1">
+                    <g id="telephone">
+                        <image
+                            id="reciever"
+                            width={1314}
+                            height={441}
+                            style={{transform: "scale(0.24)"}}
+                            xlinkHref={reciever}
+                            onLoad={handleImageLoad}
+                        />
+                        <image
+                            id="body"
+                            width={1247}
+                            height={1156}
+                            style={{transform: "translate(8.08px, 38.66px) scale(0.24)"}}
+                            xlinkHref={body}
+                            onLoad={handleImageLoad}
+                        />
+                        {[...Array(9)].map((_, i) => (
+                            <image
+                                key={i}
+                                id={`btn-${i + 1}`}
+                                width={137}
+                                height={142}
+                                style={{
+                                    transform: `translate(${80 + (i % 3) * 60}px, ${
+                                        123 + Math.floor(i / 3) * 41
+                                    }px) scale(0.24)`,
+                                }}
+                                xlinkHref={btn}
+                                onLoad={handleImageLoad}
+                            />
+                        ))}
+                    </g>
                 </g>
-            </g>
-        </svg>
+            </svg>
+        </div>
     );
-}
+};
 
 const ForwardRef = forwardRef(SvgComponent);
 export default ForwardRef;
-
